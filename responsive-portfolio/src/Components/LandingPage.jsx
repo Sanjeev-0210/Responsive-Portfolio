f98af74716a6117c47b318a4e020fb7b5ae9af1d
Sanjeev C <sanjeev0210c@gmail.com>
2024-05-20 16:50:55 +0530

    Adding LandingPage

---
+++ responsive-portfolio/src/Components/LandingPage.jsx
@@ -0,0 +1,13 @@
+import '../Styles/LandingPage.css'
+const LandingPage = () => {
+    return ( 
+        <div className="landingpage">
+        <div className="navbar"><h1>Navbar</h1></div>
+        <div className="container">
+            <h1>Body</h1>
+        </div>
+        </div>
+     );
+}
+ 
+export default LandingPage;
