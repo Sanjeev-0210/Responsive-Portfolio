947d468fcb67c2ea4016c3758cc54cd389b34296
Sanjeev C <sanjeev0210c@gmail.com>
2024-07-01 17:15:04 +0530

    Added Navbar

--- responsive-portfolio/src/Components/LandingPage.jsx
+++ responsive-portfolio/src/Components/LandingPage.jsx
@@ -2,7 +2,17 @@ import '../Styles/LandingPage.css'
 const LandingPage = () => {
     return ( 
         <div className="landingpage">
-        <div className="navbar"><h1>Navbar</h1></div>
+        <div className="navbar">
+            <div id='logo'><h2>sanjeev.</h2></div>
+            <div id='links'>
+                <a href=''>Home</a>
+                <a href=''>About</a>
+                <a href=''>Resume</a>
+                <a href=''>Skills</a>
+                <a href=''>Projects</a>
+                <a href=''>Contact</a>
+            </div>
+        </div>
         <div className="container">
             <h1>Body</h1>
         </div>
