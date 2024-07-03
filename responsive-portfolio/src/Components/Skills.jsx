a3fbef75209373438a628274edde7e5f2be58206
Sanjeev C <sanjeev0210c@gmail.com>
2024-07-03 16:23:00 +0530

    Added Skills page

---
+++ responsive-portfolio/src/Components/Skills.jsx
@@ -0,0 +1,82 @@
+const Skills = () => {
+    return ( 
+        <div className="skills">
+            <h1>My Skills</h1>
+            <i>"The beautiful thing about learning is that nobody can take it away from you." - B.B. King</i>
+            <div>
+                <div className="fe-content">
+                    <h3>Java</h3>
+                    <h3>80%</h3>
+                </div>
+                <div className="se-line">
+                    <div></div>
+                </div>
+            </div>
+            <div>
+            <div className="fe-content">
+                    <h3>Javascript</h3>
+                    <h3>85%</h3>
+                </div>
+                <div className="se-line">
+                    <div></div>
+                </div>
+            </div>
+            <div>
+            <div className="fe-content">
+                    <h3>HTML & CSS</h3>
+                    <h3>90%</h3>
+                </div>
+                <div className="se-line">
+                    <div></div>
+                </div>
+            </div>
+            <div>
+            <div className="fe-content">
+                    <h3>ReactJS</h3>
+                    <h3>80%</h3>
+                </div>
+                <div className="se-line">
+                    <div></div>
+                </div>
+            </div>
+            <div>
+            <div className="fe-content">
+                    <h3>SQL</h3>
+                    <h3>80%</h3>
+                </div>
+                <div className="se-line">
+                    <div></div>
+                </div>
+            </div>
+            <div>
+            <div className="fe-content">
+                    <h3>SpringBoot</h3>
+                    <h3>85%</h3>
+                </div>
+                <div className="se-line">
+                    <div></div>
+                </div>
+            </div>
+            <div>
+            <div className="fe-content">
+                    <h3>Python</h3>
+                    <h3>70%</h3>
+                </div>
+                <div className="se-line">
+                    <div></div>
+                </div>
+            </div>
+            <div>
+            <div className="fe-content">
+                    <h3>Adobe XD</h3>
+                    <h3>75%</h3>
+                </div>
+                <div className="se-line">
+                    <div></div>
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default Skills;
