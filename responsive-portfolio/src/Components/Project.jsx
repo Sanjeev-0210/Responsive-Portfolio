cf7daaf06c900f920c4bf76bd6e006655f042740
Sanjeev C <sanjeev0210c@gmail.com>
2024-07-04 15:16:48 +0530

    Added Project page

--- responsive-portfolio/src/Components/Project.jsx
+++ responsive-portfolio/src/Components/Project.jsx
@@ -5,10 +5,33 @@ const Project = () => {
             <h1>My Projects</h1>
             <i>"The only way to do great work is to love what you do." - Steve Jobs</i>
             <div id="container">
-                <div id="foodcart"></div>
-                <div id="busapp"></div>
-                <div id="portfolio"></div>
-                <div id="netflix"></div>
+            <div>
+                <div id="foodcart">
+                    <div id="sub-content">
+                        <h2><a href="">Food Cart</a></h2>
+                        <p>Web Application</p>
+                    </div>
+                </div>
+                <div id="busapp">
+                <div id="sub-content">
+                        <h2><a href="">Bus Reservation App</a></h2>
+                        <p>Web Application</p>
+                    </div></div> 
+            </div>
+            <div>
+                <div id="portfolio">
+                <div id="sub-content">
+                        <h2><a href="">Portfolio</a></h2>
+                        <p>Web Application</p>
+                    </div>
+                </div>
+                <div id="audi">
+                <div id="sub-content">
+                        <h2><a href="">Audi Website</a></h2>
+                        <p>Clone Website</p>
+                    </div>
+                </div>
+            </div>
             </div>
         </div>
      );
